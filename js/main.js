717e78083beb0158dcaf107590cc7a460c8ede68
Sabohatfrontend <ssobirova333@gmail.com>
2021-12-05 20:07:07 +0500

    correcr js function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const elbutton = document.querySelector('.button');
 elbutton.addEventListener('click', function(){
-  document.body.classList.add('dark-mode');
-  // document.body.classList.toggle('dark-mode');
+  document.body.classList.toggle('dark-mode');
 });
