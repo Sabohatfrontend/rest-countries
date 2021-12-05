61f10b17a68536904c22014b99d57538b7d9c717
Sabohatfrontend <ssobirova333@gmail.com>
2021-12-05 19:41:34 +0500

    Add dark -mode js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const elbutton = document.querySelector('.button');
+elbutton.addEventListener('click', function(){
+  document.body.classList.add('dark-mode');
+  // document.body.classList.toggle('dark-mode');
+});
